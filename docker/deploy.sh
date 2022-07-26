16a5f1f5e5386ce8caf47efb62ccf2e3589661b4
Chris <christopher.thierauf@tufts.edu>
2022-07-26 17:24:45 -0400

    dockerfile no longer produces exec error

---
+++ docker/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+/bin/bash -c 'cd .. && 				\
+		docker run -it --rm --privileged --net=host  	\
+		    --volume '/home/cst/git/spot_ros_nav/config:/config:ro'\
+		    --volume '/home/cst/git/spot_ros_nav/maps:/maps:rw'    \
+		    --volume '/tmp/.X11-unix:/tmp/.X11-unix:rw' \
+		    --volume '/etc/localtime:/etc/localtime:ro' \
+		    spot_ros_nav /bin/bash'
