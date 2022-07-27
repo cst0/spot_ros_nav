b47f149b019340dc3864ae63ff28aefe2b69c753
Chris Thierauf <christopher.thierauf@tufts.edu>
2022-07-27 12:42:37 -0400

    navigation debug on robot

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -2,8 +2,8 @@
 
 /bin/bash -c 'cd .. && 				\
 		docker run -it --rm --privileged --net=host  	\
-		    --volume '/home/cst/git/spot_ros_nav/config:/config:ro'\
-		    --volume '/home/cst/git/spot_ros_nav/maps:/maps:rw'    \
+		    --volume '/home/cst/ws_spot/src/spot_ros_nav/config:/config:ro'\
+		    --volume '/home/cst/ws_spot/src/spot_ros_nav/maps:/maps:rw'    \
 		    --volume '/tmp/.X11-unix:/tmp/.X11-unix:rw' \
 		    --volume '/etc/localtime:/etc/localtime:ro' \
 		    spot_ros_nav /bin/bash'
