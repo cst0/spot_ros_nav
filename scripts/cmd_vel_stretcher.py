e2343aa489f776f6468c79228b4933cf96d8b5c4
Chris Thierauf <christopher.thierauf@tufts.edu>
2022-07-27 13:41:26 -0400

    cmd_vel_stretcher now sanity checks

--- scripts/cmd_vel_stretcher.py
+++ scripts/cmd_vel_stretcher.py
@@ -10,6 +10,7 @@ the spot has a very high cutoff for input command velocity rates:
 
 import rospy
 from geometry_msgs.msg import Twist
+from rospy.timer import TimerEvent
 
 class CmdVelStretcher:
     def __init__(self):
@@ -17,13 +18,24 @@ class CmdVelStretcher:
         self.cmd_vel_pub = rospy.Publisher('/spot/cmd_vel', Twist, queue_size=1)
         self.most_recent = None
         self.timer = rospy.Timer(rospy.Duration(0.1), self.timer_cb)
+        self.timings = []
+        self.last_time = rospy.Time.now()
 
     def cmd_vel_cb(self, msg):
+        self.timings.append(rospy.Time.now() - self.last_time)
+        if len(self.timings) > 10:
+            self.timings.pop(0)
+        self.last_time = rospy.Time.now()
+
         self.most_recent = msg
 
     def timer_cb(self, _):
-        if self.most_recent is not None:
-            self.cmd_vel_pub.publish(self.most_recent)
+        # check the average times
+        avg = sum(self.timings) / len(self.timings)
+        # if the last time is within 20% of the average, then we're good to publish
+        if self.last_time - rospy.Time.now() < avg * 0.8:
+            if self.most_recent is not None:
+                self.cmd_vel_pub.publish(self.most_recent)
 
 if __name__ == '__main__':
     rospy.init_node('cmd_vel_stretcher')
