e5e8cb8840eab09245ad40f79426588fde11a94e
Chris Thierauf <christopher.thierauf@tufts.edu>
2022-08-17 12:15:08 -0400

    bugfix cmd_vel_stretcher

--- scripts/cmd_vel_stretcher.py
+++ scripts/cmd_vel_stretcher.py
@@ -10,40 +10,35 @@ the spot has a very high cutoff for input command velocity rates:
 
 import rospy
 from geometry_msgs.msg import Twist
+from rospy.timer import TimerEvent
 
 class CmdVelStretcher:
     def __init__(self):
         self.cmd_vel_sub = rospy.Subscriber('cmd_vel', Twist, self.cmd_vel_cb)
         self.cmd_vel_pub = rospy.Publisher('spot/cmd_vel', Twist, queue_size=1)
         self.most_recent = None
-        self.timer = rospy.Timer(rospy.Duration(1/40), self.timer_cb)
+        self.timer = rospy.Timer(rospy.Duration(1/30), self.timer_cb)
         self.timings = []
-        self.last_time = rospy.Time.now()
+        self.last_time = rospy.Time.now().to_sec()
 
     def cmd_vel_cb(self, msg:Twist):
-        #if abs(msg.linear.x) < 0.1 \
-        #    and abs(msg.linear.y) < 0.1 \
-        #    and abs(msg.angular.z) < 0.1:
-        #    return
-
-        self.timings.append(rospy.Time.now() - self.last_time)
+        self.timings.append(rospy.Time.now().to_sec() - self.last_time)
         if len(self.timings) > 10:
             self.timings.pop(0)
-        self.last_time = rospy.Time.now()
+        self.last_time = rospy.Time.now().to_sec()
 
         self.most_recent = msg
 
-    def timer_cb(self, _):
-        # check the average times
-        if len(self.timings) == 0:
+    def timer_cb(self, event:TimerEvent):
+        if event.last_real is None or self.most_recent is None or len(self.timings) == 0:
+            # no useful data yet
             return
 
-        self.cmd_vel_pub.publish(self.most_recent)
-        #avg = sum(self.timings) / len(self.timings)
-        ## if the last time is within 20% of the average, then we're good to publish
-        #if self.last_time - rospy.Time.now() > avg * 0.8:
-        #    if self.most_recent is not None:
-        #        self.cmd_vel_pub.publish(self.most_recent)
+        avg_subscription_rate = (sum(self.timings) / len(self.timings))
+        # use the TimerEvent to determine if it's been too long since we last saw a message
+        if rospy.Time.now().to_sec() - self.last_time < avg_subscription_rate:
+            # if so, just publish the most recent message
+            self.cmd_vel_pub.publish(self.most_recent)
 
 if __name__ == '__main__':
     rospy.init_node('cmd_vel_stretcher')
